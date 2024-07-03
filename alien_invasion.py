bed8de83043c23a11e25fc0bdede508cccb7104b
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-07-03 08:51:19 -0300

    settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
-    #manage game assets and behavior#
+    #classe para gerenciar assets e comportamento
 
     def __init__(self):
+        #inicia jogo e recursos
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            self.settings.screen.widht, self.settings.screen.height
+        )
         pygame.display.set_caption("Alien Invasion")
+        #cor do background
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
 
@@ -18,3 +26,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.settings.bg.color)
+            pygame.display.flip()
+if __name__ == '__main__':
+    #rodar o jogo
+    ai = AlienInvasion()
+    ai.run_game
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+
+    def __init__(self):
+        #configuracoes do jogo
+
+        self.screen.width = 1200
+        self.screen.height = 800
+        self.bg.color = (230, 230, 230)
