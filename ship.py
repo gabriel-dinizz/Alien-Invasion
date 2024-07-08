ab72c069611e514f4b3c79e8bbd425a0c14a5e50
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-07-08 11:16:37 -0300

    left and right mov

--- ship.py
+++ ship.py
@@ -19,6 +19,16 @@ class Ship:
         # Coloca a nave na parte inferior central da tela
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # Atualiza a posição da nave com base na flag de movimento
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         # Desenha a nave na tela na posição atual
         self.screen.blit(self.image, self.rect)
