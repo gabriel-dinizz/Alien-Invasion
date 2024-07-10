b050e150bf6c8534927c2d2b660de79f2abd3147
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-07-10 09:04:39 -0300

    adding bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet   
 
 class AlienInvasion:
     # Manage assets and behavior
@@ -16,12 +17,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-        self.bg_color = self.settings.bg_color  # Certifique-se de definir a cor de fundo
+        self.bullets = pygame.sprite.Group()
+        self.bg_color = self.settings.bg_color
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -40,16 +43,33 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+    
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        # Update bullet positions
+        self.bullets.update()
+
+        # Remove bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
 class Settings:
     def __init__(self):
-        # Configurações do jogo
+        # Game settings
         self.ship_speed = 1.5   
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (135,206,250)
+        self.bg_color = (135, 206, 250)
+        
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
