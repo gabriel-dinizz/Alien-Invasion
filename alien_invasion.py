fb421b6d6d7c64aafe29f3ed1cd926909d480fe1
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-08-09 11:43:11 -0300

    added fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,9 +24,19 @@ class AlienInvasion:
         self.bg_color = self.settings.bg_color
 
     def _create_fleet(self):
-        #create fleet of aliens
+    # Create a fleet of aliens
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+    
+        for alien_number in range(number_aliens_x):
+        # Create an alien and place it in a row
+         alien = Alien(self)
+         alien.x = alien_width + 2 * alien_width * alien_number
+         alien.rect.x = alien.x 
+         self.aliens.add(alien)
+
 
     def run_game(self):
         while True:
