37b7021530d063c599a9fa98cf228a61198e8c2f
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-08-10 15:31:11 -0300

    commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,8 @@ class AlienInvasion:
         # Initialize game and resources
         pygame.init()
         self.settings = Settings()
-        #fullscreen
-        self.screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
+        # Fullscreen
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height 
         pygame.display.set_caption("Alien Invasion")
@@ -24,19 +24,33 @@ class AlienInvasion:
         self.bg_color = self.settings.bg_color
 
     def _create_fleet(self):
-    # Create a fleet of aliens
+        """Create a fleet of aliens."""
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
         alien_width = alien.rect.width
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
-    
-        for alien_number in range(number_aliens_x):
-        # Create an alien and place it in a row
-         alien = Alien(self)
-         alien.x = alien_width + 2 * alien_width * alien_number
-         alien.rect.x = alien.x 
-         self.aliens.add(alien)
 
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                        (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+    
+        for row_number in range(number_rows):    
+          for alien_number in range(number_aliens_x):
+            # Create an alien and place it in a row
+            self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Create an alien and position it in the row."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x 
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def run_game(self):
         while True:
@@ -71,9 +85,9 @@ class AlienInvasion:
             self.ship.moving_left = False
     
     def _fire_bullet(self):
-      if len(self.bullets) < self.settings.bullets_allowed:
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_bullets(self):
         # Update bullet positions
